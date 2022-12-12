9dabd580a1b21c51e6f125d3c8f70bd02a7aec7a
chaz-n <chaz-w-neal@hotmail.co.uk>
2022-12-12 15:45:04 +0000

    removed tutorial content

--- mysite/blog/urls.py
+++ mysite/blog/urls.py
@@ -2,10 +2,7 @@ from . import views
 from django.urls import path, include
 
 urlpatterns = [
-    path('', views.PostList.as_view(), name='home'),
-
+    path('', views.home, name='blog-home'),
     path('about/', views.about, name='blog-about'),
-    path('corey/', views.corey, name='blog-home'),
 
-    path('<slug:slug>/', views.PostDetail.as_view(), name='post_detail'),
 ]
--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -21,23 +21,12 @@ posts = [
 ]
 
 
-class PostList(generic.ListView):
-    queryset = Post.objects.filter(status=1).order_by('-created_on')
-    template_name = 'index.html'
-    paginate_by = 8
-
-
-class PostDetail(generic.DetailView):
-    model = Post
-    template_name = 'post_detail.html'
-
-
-def corey(request):
+def home(request):
     context = {
         'posts': posts
     }
-    return render(request, 'test.html', context)
+    return render(request, 'index.html', context)
     
 
 def about(request):
-    return HttpResponse('<h1>Blog About</h1>')
+    return render(request, 'about.html', {'title': 'About'})
