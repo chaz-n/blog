381bd526a1f7fb414166f891e35eacc70a342899
chaz-n <chaz-w-neal@hotmail.co.uk>
2022-12-23 13:27:00 +0000

    further tutorial

--- mysite/blog/admin.py
+++ mysite/blog/admin.py
@@ -3,11 +3,11 @@ from .models import Post
 from django_summernote.admin import SummernoteModelAdmin
 
 
-class PostAdmin(admin.ModelAdmin):
-    list_display = ('title', 'slug', 'status','created_on')
-    list_filter = ("status",)
-    search_fields = ['title', 'content']
-    prepopulated_fields = {'slug': ('title',)}
+# class PostAdmin(admin.ModelAdmin):
+#     list_display = ('title', 'slug', 'status','created_on')
+#     list_filter = ("status",)
+#     search_fields = ['title', 'content']
+#     prepopulated_fields = {'slug': ('title',)}
 
 
 class PostAdmin(SummernoteModelAdmin):
--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -3,26 +3,9 @@ from .models import Post
 
 from django.shortcuts import render
 
-
-posts = [
-    {
-        'author': 'chaz',
-        'title': 'dummy post',
-        'content': 'First post content',
-        'date_posted': 'Today'
-    },
-    {
-        'author': 'chaz2',
-        'title': 'dummy post2',
-        'content': 'Second post content',
-        'date_posted': 'Yesterday'
-    }
-]
-
-
 def home(request):
     context = {
-        'posts': posts
+        'posts': Post.objects.all(),
     }
     return render(request, 'blog/index.html', context)
     
