9122bffd82774ac7bee5205e81029a67a31e1bbd
chaz-n <chaz-w-neal@hotmail.co.uk>
2023-01-03 15:44:10 +0000

    no

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,8 @@ TEMPLATES_DIRS = os.path.join(BASE_DIR, 'templates')
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-xqofukcmu#glzkce_i__4+_oxd0+d3mg*yj2b@3!kfhsk)7pei'
+SECRET_KEY = os.environ.get('SECRET_KEY')
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
