4b1824141688760d4f94a22c95e58adb32de269e
chaz-n <chaz-w-neal@hotmail.co.uk>
2023-01-06 14:57:45 +0000

    changed app name

--- scripts/run.sh
+++ scripts/run.sh
@@ -8,4 +8,4 @@ python manage.py collectstatic --noinput
 
 # uwsgi --socket :9000 --master --enable-threads --module app.wsgi
 
-uwsgi --socket :9000 --workers  --master --enable-threads --module app.wsgi
+uwsgi --socket :9000 --workers  --master --enable-threads --module mysite.wsgi
