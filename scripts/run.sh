82e6a316d1446f780d8e6c9128d6fc6c706bc5b8
chaz-n <chaz-w-neal@hotmail.co.uk>
2023-01-08 00:51:04 +0000

    fixed uwsgi command

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,5 +1,5 @@
-#!/bin/bash
+#!/bin/sh
 
-python manage.py collectstaic  --noinput
+python manage.py collectstatic  --noinput
 
-uwsgi --socket :9000 --workers  --master --enable-threads --module mysite.wsgi
+uwsgi --socket :9000 --master --enable-threads --module mysite.wsgi
