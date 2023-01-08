f82d6ac9f4aedea3e8bd319a47b85426cf798ff9
chaz-n <chaz-w-neal@hotmail.co.uk>
2023-01-08 16:50:38 +0000

    added csrf token origins list, will be needed

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -32,6 +32,7 @@ SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY", "setmeinprod")
 DEBUG = int(os.environ.get("DJANGO_DEBUG", default=0))
 
 ALLOWED_HOSTS = [] if DEBUG else os.environ.get("DJANGO_ALLOWED_HOSTS").split(',')
+CSRF_TRUSTED_ORIGINS = []
 
 # Application definition
 
