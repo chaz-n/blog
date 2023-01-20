7bd7db2212e15bf14cc98639202b3aec59682fb1
chaz-n <chaz-w-neal@hotmail.co.uk>
2023-01-20 16:34:25 +0000

    added filters and post status functions to admin site

--- mysite/blog/admin.py
+++ mysite/blog/admin.py
@@ -3,15 +3,18 @@ from .models import Post
 from django_summernote.admin import SummernoteModelAdmin
 
 
-# class PostAdmin(admin.ModelAdmin):
-#     list_display = ('title', 'slug', 'status','created_on')
-#     list_filter = ("status",)
-#     search_fields = ['title', 'content']
-#     prepopulated_fields = {'slug': ('title',)}
+@admin.action(description='Mark as published')
+def make_published(ModelAdmin, request, queryset):
+    queryset.update(status=1)
 
+@admin.action(description='Mark as drafts')
+def make_draft(ModelAdmin, request, queryset):
+    queryset.update(status=0)
 
 class PostAdmin(SummernoteModelAdmin):
     summernote_fields = ('content',)
+    list_filter = ("updated_on", "created_on", "status", "author_id")
+    actions = [make_published, make_draft]
 
 
 admin.site.register(Post, PostAdmin)
