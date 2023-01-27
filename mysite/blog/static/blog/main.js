7ac5a35041583556f8d3e2b73dbda09a0db3914a
chaz-n <chaz-w-neal@hotmail.co.uk>
2023-01-27 20:27:29 +0000

    first attempt at light mode / dark mode

---
+++ mysite/blog/static/blog/main.js
@@ -0,0 +1,22 @@
+const storedTheme = localStorage.getItem('theme')
+
+const getPreferredTheme = () => {
+    if (storedTheme) {
+        return storedTheme
+    }
+
+    return window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light'
+}
+
+const setTheme = function (theme) {
+    if (theme === 'auto' && window.matchMedia('(prefers-color-scheme: dark)').matches) {
+        document.documentElement.setAttribute('data-bs-theme', 'dark')
+        localStorage.setItem('theme', theme)
+
+    } else {
+        document.documentElement.setAttribute('data-bs-theme', theme)
+        localStorage.setItem('theme', theme)
+    }
+}
+
+setTheme(getPreferredTheme())
